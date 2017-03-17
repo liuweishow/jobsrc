0e26d0dd364cd33d4275a90ee4ba6a76922be8e5
liuweishow <superman321>
2017-03-17 19:52:10 +0800

    fff

---
+++ QuartJobApp/QuartzTH/FirstJob1.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzTH
+{
+    public sealed class FirstJob1 : IJob
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(FirstJob0));
+        public void Execute(IJobExecutionContext context)
+        {
+          
+            logger.InfoFormat("my first job1！");
+        }
+    }
+}
---
+++ QuartJobApp/QuartzTH/GetConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzTH
+{
+    public class GetConfig : ConfigurationSection
+    {
+        [ConfigurationProperty("UserName")]
+        public string UserName
+        {
+            get { return this["UserName"].ToString(); }
+            set { this["UserName"] = value; }
+        }
+
+        [ConfigurationProperty("Path")]
+        public string Path
+        {
+            get { return this["Path"].ToString(); }
+            set { this["Path"] = value; }
+        }
+    }
+}
